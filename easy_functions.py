5b0fedc34e906b7a61622f57318987762970a3a2
anothermartz <89461919+anothermartz@users.noreply.github.com>
2023-09-06 10:21:22 +0100

    Update easy_functions.py

--- easy_functions.py
+++ easy_functions.py
@@ -31,3 +31,48 @@ def show_video(file_path):
       <source src="%s" type="video/mp4">
   </video>
   """ % data_url))
+
+def format_time(seconds):
+    hours = int(seconds // 3600)
+    minutes = int((seconds % 3600) // 60)
+    seconds = int(seconds % 60)
+
+    if hours > 0:
+        return f'{hours}h {minutes}m {seconds}s'
+    elif minutes > 0:
+        return f'{minutes}m {seconds}s'
+    else:
+        return f'{seconds}s'
+
+def _load(checkpoint_path):
+    if device == 'cuda':
+        checkpoint = torch.load(checkpoint_path)
+    else:
+        checkpoint = torch.load(checkpoint_path,
+                                map_location=lambda storage, loc: storage)
+    return checkpoint
+
+def load_model(path):
+    model = Wav2Lip()
+    print("Load checkpoint from: {}".format(path))
+    checkpoint = _load(path)
+    s = checkpoint["state_dict"]
+    new_s = {}
+    for k, v in s.items():
+        new_s[k.replace('module.', '')] = v
+    model.load_state_dict(new_s)
+
+    model = model.to(device)
+    return model.eval()
+
+def get_input_length(filename):
+    result = subprocess.run(
+        ["ffprobe", "-v", "error", "-show_entries", "format=duration",
+         "-of", "default=noprint_wrappers=1:nokey=1", filename],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT)
+    return float(result.stdout)
+
+def is_url(string):
+    url_regex = re.compile(r'^(https?|ftp)://[^\s/$.?#].[^\s]*$')
+    return bool(url_regex.match(string))
