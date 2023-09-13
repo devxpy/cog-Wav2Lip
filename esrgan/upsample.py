5094142b20b67a0db7ceac9247aa2ea2f783b025
anothermartz <89461919+anothermartz@users.noreply.github.com>
2023-09-13 14:46:57 +0100

    Update upsample.py

--- esrgan/upsample.py
+++ esrgan/upsample.py
@@ -6,9 +6,7 @@ from gfpgan import GFPGANer
 warnings.filterwarnings("ignore")
 
 def load_sr(model_path, device, face):
-  model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4) #alter to match dims as needed
-  model_path = os.path.normpath(model_path)
-  run_params=gfp = GFPGANer(
+  run_params = GFPGANer(
       model_path='https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth',
       upscale=1,
       arch='clean',
@@ -19,5 +17,5 @@ def load_sr(model_path, device, face):
 
 
 def upscale(image, face, properties):
-      _, _, output = properties.enhance(image, has_aligned=False, only_center_face=True, paste_back=True)
+      _, _, output = properties.enhance(image, has_aligned=False, only_center_face=False, paste_back=True)
       return output
