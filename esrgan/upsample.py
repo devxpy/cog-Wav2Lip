940107e155d67e9e39b9a19392fe2a0e88f15180
anothermartz <89461919+anothermartz@users.noreply.github.com>
2023-09-13 21:14:14 +0100

    Update upsample.py

--- esrgan/upsample.py
+++ esrgan/upsample.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import warnings
 from basicsr.archs.rrdbnet_arch import RRDBNet
@@ -5,24 +6,27 @@ from gfpgan import GFPGANer
 
 warnings.filterwarnings("ignore")
 
-def load_sr(model_path, device, face):
-  run_params = GFPGANer(
+def load_sr(model):
+  if model = 'gfpgan':
+    run_params = GFPGANer(
       model_path='https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth',
       upscale=1,
       arch='clean',
       channel_multiplier=2,
       bg_upsampler=None)
-
-  '''run_params = GFPGANer(
+  elif model = 'RestoreFormer':
+    run_params = GFPGANer(
       model_path='https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/RestoreFormer.pth',
       upscale=1,
       arch='RestoreFormer',
       channel_multiplier=2,
-      bg_upsampler=None)'''
+      bg_upsampler=None)
+  else:
+    sys.exit('load_sr model not supported - must be gfpgan or RestoreFormer')    
   
   return run_params
 
 
-def upscale(image, face, properties):
+def upscale(image, properties):
       _, _, output = properties.enhance(image, has_aligned=False, only_center_face=False, paste_back=True)
       return output
